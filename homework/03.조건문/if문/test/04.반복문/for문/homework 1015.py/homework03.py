be7c44577066a0209ceba5bc65ce5d180dfc0943
kke0724 <a01083504644@gmail.com>
2025-10-15 23:22:11 +0900

    김강은

---
+++ homework/03.조건문/if문/test/04.반복문/for문/homework 1015.py/homework03.py
@@ -0,0 +1,6 @@
+a = 0
+
+for i in range(1,101):
+    if i%4 == 0:
+        a = a +i
+        print(f'{i} --> {a}')
---
+++ homework/03.조건문/if문/test/04.반복문/for문/test/test05.py
@@ -0,0 +1,9 @@
+number = input("숫자를 입력하세요")
+
+total = 0
+
+for a in range (len(number)):
+    a = int(a)
+    if a%2 == 1:
+      total +=1
+print("홀수의 개수 : %d개" % total)
