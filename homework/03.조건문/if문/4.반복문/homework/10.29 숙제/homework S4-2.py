f2c823147ea091ce465b9e4bd94f428f42c676bd
kke0724 <a01083504644@gmail.com>
2025-10-30 14:07:46 +0900

    숙제 수정

--- homework/03.조건문/if문/4.반복문/homework/10.29 숙제/homework S4-2.py
+++ homework/03.조건문/if문/4.반복문/homework/10.29 숙제/homework S4-2.py
@@ -1,5 +1,6 @@
 i = int(input('성적을 입력하시오:'))
 while i != 'q':
+    i = int(input('성적을 입력하시오:'))
     if i>= 90:
         print('등급:수')
     elif i>= 80:
@@ -11,8 +12,8 @@ while i != 'q':
     else:
         print('등급:가')
 
-    z = int(input('계속하시겠습니까?(중단:q,계속:y)'))
+    z = int(input('계속하시겠습니까?(중단:q,계속:y):'))
     if z == 'q':
             break
 
-i = int(input('성적을 입력하시오:'))
+   
